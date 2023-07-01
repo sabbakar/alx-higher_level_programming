14b9e3b21363d20082dbb2b01e4ac6d2b2baf0a4
sabbakar <sadeeqsas14@gmail.com>
2023-07-01 12:54:50 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for c in range(i + 1, 10):
         if i < 88:
-            print("{}{},".format(i, c), end="")
+            print("{}{}, ".format(i, c), end="")
         else:
             print("{}{}".format(i, c))
