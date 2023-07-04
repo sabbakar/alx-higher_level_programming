fa068789ec7089cf27e50802141298f432e765f0
sabbakar <sadeeqsas14@gmail.com>
2023-07-04 13:48:53 -0700

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
     elif len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
