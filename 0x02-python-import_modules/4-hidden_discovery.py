73842b1ce596b62466dae14751ae18024fe971ae
sabbakar <sadeeqsas14@gmail.com>
2023-07-04 07:21:39 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import hidden_4
-
+    import hidden_4
+    hidden = dir(hidden_4)
+    for name in hidden:
+        if not name.startswith("__"):
+            print(name)
