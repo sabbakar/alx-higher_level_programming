59d2394245b5fcee0938227f87adae7dfb276085
sabbakar <sadeeqsas14@gmail.com>
2023-07-08 08:29:24 -0700

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     if idx < 0:
         return copy
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return copy
     copy[idx] = element
     return copy
