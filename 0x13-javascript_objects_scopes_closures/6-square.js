9523fef57accd3e817b189504116fe90a544683a
sabbakar <sadeeqsas14@gmail.com>
2023-09-26 22:11:27 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const PrevSquare = require('./5-square.js');
+
+class Square extends PrevSquare {
+  charPrint (c) {
+    const myChar = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += myChar;
+        y++;
+      }
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Square;
