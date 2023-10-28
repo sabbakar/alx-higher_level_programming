2c7b6632e32c24d852f01214c699f91a1523c428
sabbakar <sadeeqsas14@gmail.com>
2023-10-28 22:07:38 +0100

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-[ "$response" -eq 200 ] && curl -s "$1"
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
