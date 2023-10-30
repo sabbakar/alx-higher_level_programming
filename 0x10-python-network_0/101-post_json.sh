43cb5b9a9cc57ca75e7b5eaaddb8b12dd9563b8f
sabbakar <sadeeqsas14@gmail.com>
2023-10-30 03:21:49 +0100

    Task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to post a JSON content to a server
-curl -X POST -H "Content-Type: application/json"  -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type: application/json"  -d "$(cat "$2")" "$1"
