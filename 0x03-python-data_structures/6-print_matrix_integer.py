59ba52b0434b3da4ee24a62b040bd88f7c0a1337
sabbakar <sadeeqsas14@gmail.com>
2024-09-02 15:13:00 +0100

    back to alx python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,3 +4,13 @@ def print_matrix_integer(matrix=[[]]):
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
+
+#matrix = [
+   # [1, 2, 3],
+  #  [4, 5, 6],
+ #   [7, 8, 9]
+#]
+
+#print_matrix_integer(matrix)
+#print("--")
+#print_matrix_integer()
