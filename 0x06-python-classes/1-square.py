81091e5418beda02f140fd9049a05f8105025754
sabbakar <sadeeqsas14@gmail.com>
2024-09-02 18:33:02 +0100

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a module to define a Square class with private size attribute.
+"""
+
+
+class Square:
+    """
+    a class to define a square with a private size attribute.
+    """
+    def __init__(self, __size):
+        """
+        A method that initializes the square instance to accept size
+        """
+        return None
