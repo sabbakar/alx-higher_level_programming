b85e05d957984a9ac8972cdab83c2a08076fdeb0
sabbakar <sadeeqsas14@gmail.com>
2024-09-07 00:35:32 +0100

    task 4

---
+++ 0x06-python-classes/4a-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A module that defines a square class
+"""
+
+
+class Square:
+    """
+    a class Square that defines a square by: 
+    (based on 3-square.py)
+    """
+    def __init__(self, size=0):
+        """
+        a method of Private instance attribute: size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must br >= 0")
+        self.__size = size
+    
+    @property
+    def size(self):
+        """
+        a getter method to retrieve the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        a setter method to set a size
+        """
+        self.__size = value
+
+    def area(self):
+        """
+        method that returns the current square area
+        """
+        return self.__size * self.__size
