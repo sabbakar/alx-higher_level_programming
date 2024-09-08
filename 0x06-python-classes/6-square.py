7428d96c971da382f326e380d6e4fcfe2eb0e193
sabbakar <sadeeqsas14@gmail.com>
2024-09-08 08:48:14 +0100

    task 6 correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,8 +19,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
