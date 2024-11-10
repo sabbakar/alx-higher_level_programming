5244ad9b5a17d793e5498b4b98d4f2ee4b4602fe
sabbakar <sadeeqsas14@gmail.com>
2024-11-11 00:40:06 +0100

    correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python3
-'''Contains an add_integer function for a TDD project.
-'''
+"""
+A module that has  a function that adds 2 integers.
+"""
 
 
 def add_integer(a, b=98):
-    '''Computes the sum of two integers.
+    """
+    A function that adds 2 integers.
+    """
 
-    Args:
-        a (int): The first number.
-        b (int, optional): The second number.
-
-    Returns:
-        int: The sum of the two integers.
-    '''
-    if not isinstance(a, (int, float)):
-        raise TypeError('a must be an integer')
-    elif not isinstance(b, (int, float)):
-        raise TypeError('b must be an integer')
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
     else:
-        return int(a) + int(b)
+        a = int(a)
+        b = int(b)
+        return a + b
 
