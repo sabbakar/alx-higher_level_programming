65fb37dc5e99a576777240e4cf3f397521e61cd5
sabbakar <sadeeqsas14@gmail.com>
2024-11-30 11:20:50 +0100

    another task 4 correction

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-A module for  a function that returns True if
-the object is an instance of a class that inherited (directly orindirectly)
-from the specified class ; otherwise False.
-"""
+'''A module for inspecting an object.
+'''
 
 
 def inherits_from(obj, a_class):
-    """
-    A module for  a function that returns True if
-    the object is an instance of a class that inherited (directly orindirectly)
-    from the specified class ; otherwise False.
-    """
+    '''Checks if an object inherited from the specified class.
 
+    Args:
+        obj (any): an object to be checked.
+        a_class (any): A class to be compared against.
+
+    Returns:
+        bool: True if `obj` is an inheritance of `a_class`.
+    '''
     return issubclass(obj.__class__, a_class) and (type(obj) is not a_class)
