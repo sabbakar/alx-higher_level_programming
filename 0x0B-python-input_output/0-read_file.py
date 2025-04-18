bf9de3d9c94465cfdc4bd50cad38d925eb1627ad
sabbakar <sadeeqsas14@gmail.com>
2025-04-18 23:22:33 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+module read file
+"""
 
 def read_file(filename=""):
+    """
+    function to read file
+    """
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
         print(read_file)
