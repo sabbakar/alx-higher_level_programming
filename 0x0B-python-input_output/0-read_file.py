7a28c124382643af77225c2c3d4d7060608ec56c
sabbakar <sadeeqsas14@gmail.com>
2025-04-18 23:32:21 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     function to read file
     """
     with open(filename, encoding="utf-8") as f:
-        read_file = f.read()
-        print(read_file)
+        print(f.read(), end="")
